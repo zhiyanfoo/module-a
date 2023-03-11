41f47913b5e9627cfdfd9e90d519d02fd73043e0
Zhiyan Foo <zhiyanfoo@gmail.com>
2023-03-11 14:56:22 -0500

    examples

--- examples/a/a.go
+++ examples/a/a.go
@@ -2,10 +2,12 @@ package main
 
 import (
 	"fmt"
+	"github.com/zhiyanfoo/module-a/lib/a"
 )
 
 func main() {
 	fmt.Printf("example a main\n")
+	a.F1()
 }
 
 func init() {
--- examples/a/go.mod
+++ examples/a/go.mod
@@ -1,3 +1,5 @@
 module github.com/zhiyanfoo/module-a/examples/a
 
 go 1.20
+
+require github.com/zhiyanfoo/module-a v0.1.0 // indirect
